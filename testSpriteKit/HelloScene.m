46d994e5104c3eaa4e16ead9dbf406a06e77d5f7
LucaEdmodo <luca.prasso@edmodo.com>
2013-12-20 14:23:20 -0800

    test

--- testSpriteKit/HelloScene.m
+++ testSpriteKit/HelloScene.m
@@ -51,7 +51,7 @@
     
     SKLabelNode *helloNode = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
     
-    helloNode.text = @"Hello, World!";
+    helloNode.text = @"Hello, Wonderful World!";
     helloNode.fontSize = 42;
     helloNode.name = @"helloNode";
     helloNode.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
